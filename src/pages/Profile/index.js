85be6797774a42cfd4805bf42100421f076079bf
Cov Developer <shusten@hotmail.com>
2020-04-02 19:53:17 -0300

    feat: added logout

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect} from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import{ FiPower, FiTrash2 } from 'react-icons/fi';
 
 import api from '../../services/api';
@@ -10,9 +10,12 @@ import logoImg from '../../assets/logo.svg';
 
 export default function Profile() {
     const [incidents, setIncidents] = useState([]);
+    
+    const history = useHistory();
 
     const ongId = localStorage.getItem('ongId');
     const ongName = localStorage.getItem('ongName');
+
 
     useEffect(() => {
         api.get('profile', {
@@ -38,6 +41,12 @@ export default function Profile() {
         }
     }
 
+    function handleLogout () {
+        localStorage.clear();
+
+        history.push('/');
+    }
+
     return (
         <div className="profile-container">
             <header>
@@ -45,7 +54,7 @@ export default function Profile() {
                 <span>Bem vinda, {ongName}</span>
             
             <Link className="button" to="/incidents/new">Cadasdrar um novo caso</Link>
-            <button type="button">
+            <button onClick={handleLogout} type="button">
                 <FiPower size={18} color="#E02041" />
             </button>
             </header>
